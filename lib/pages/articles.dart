487682b62a8923f8e6f3d7399d492e55e9166ee6
willhitman <h180128h@hit.ac.zw>
2022-01-22 15:02:58 +0200

    first commit :)

---
+++ lib/pages/articles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Articles extends StatefulWidget {
+  final int noteId;
+
+  const Articles({
+    Key? key,
+    required this.noteId,
+  }) : super(key: key);
+
+  @override
+  Articles_List createState() => Articles_List();
+}
+
+class Articles_List extends State<Articles> {
+  @override
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
